44702ade7af6a3940b1d3829281611098933be01
mcdotjs <mirectm@gmail.com>
2025-02-26 22:10:48 +0100

    read user input
    bufio Scanner

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "os"
+  "bufio"
+  "strings"
+)
 
 func main(){
-  fmt.Println("Hello, World!")
+  scanner := bufio.NewScanner(os.Stdin)
+  for ;; {
+    fmt.Print("Pokedex > ")
+    open := scanner.Scan()
+    text := scanner.Text()
+    firstWord := strings.Fields(text)[0]
+    fmt.Println("Your command was:",strings.ToLower(firstWord))
+    if open == false {
+      break
+    }
+  }
+  
 }
